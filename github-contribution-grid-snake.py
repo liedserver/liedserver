835abf9a492af2cd865e0c0734f00a3019b170b6
Liedson Saraiva <121692612+liedserver@users.noreply.github.com>
2023-03-09 15:48:18 -0300

    Create github-contribution-grid-snake.py

---
+++ github-contribution-grid-snake.py
@@ -0,0 +1,16 @@
+import datetime
+import numpy as np
+from PIL import Image
+
+def generate_image(width, height, date):
+    pixels = np.zeros((height, width), dtype=np.uint8)
+    for i in range(width):
+        for j in range(height):
+            day = datetime.date.today() - datetime.timedelta(days=(i * height + j))
+            if day.strftime('%Y-%m-%d') == date:
+                pixels[j][i] = 255
+    img = Image.fromarray(pixels)
+    img = img.resize((width * 10, height * 10), resample=Image.NEAREST)
+    img.save('snake.gif', optimize=True, quality=95)
+
+generate_image(52, 7, '2021-03-08')
