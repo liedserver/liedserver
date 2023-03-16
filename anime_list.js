60e970017d51fe7732a15f6d5c2a0383828e8447
Liedson Saraiva <121692612+liedserver@users.noreply.github.com>
2023-03-16 15:23:54 -0300

    Update anime_list.js

--- anime_list.js
+++ anime_list.js
@@ -11,6 +11,27 @@ fetch(`https://api.myanimelist.net/v2/users/${username}/animelist`, {
   let animeList = data.data;
 
   // Exiba suas informações do MyAnimeList em seu perfil do GitHub
-  console.log(animeList);
+  let animeListElement = document.getElementById('anime-list');
+
+animeList.forEach(anime => {
+  let animeCard = document.createElement('div');
+  animeCard.classList.add('anime-card');
+
+  let animeImage = document.createElement('img');
+  animeImage.src = anime.node.main_picture.medium;
+
+  let animeTitle = document.createElement('h3');
+  animeTitle.textContent = anime.node.title;
+
+  let animeEpisodes = document.createElement('p');
+  animeEpisodes.textContent = `Episódios assistidos: ${anime.progress}`;
+
+  animeCard.appendChild(animeImage);
+  animeCard.appendChild(animeTitle);
+  animeCard.appendChild(animeEpisodes);
+
+  animeListElement.appendChild(animeCard);
+});
+
 })
 .catch(error => console.error(error));
